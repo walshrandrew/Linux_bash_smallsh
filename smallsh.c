fc4416741250ea022ca65f1fd0a5a5ff9c80d4f0
Andrew Walsh <154630935+walshrandrew@users.noreply.github.com>
2025-02-19 23:14:31 +0000

    got built ins done possibly

--- smallsh.c
+++ smallsh.c
@@ -9,6 +9,8 @@
 #define INPUT_LENGTH 2048
 #define MAX_ARGS 512
 
+int status = 0;
+
 struct command_line
 {
     char *argv[MAX_ARGS + 1];
@@ -46,14 +48,40 @@ struct command_line *parse_input()
     return curr_command;
 }
 
+
 void built_in_exit(){
     // kill background process if need be when exiting
     // Maybe free mem too? prob not. if need be you will need to have curr_command as a function input
     exit(0);
 }
 
-void built_in_cd(struct command_line *curr_command);
-//void built_in_status();
+
+void built_in_cd(struct command_line *curr_command){
+    //char cwd[MAX_ARGS];
+    //char *curr_dir = getcwd(cwd, sizeof(cwd));
+    //printf("Directory before function if statememnts: %s\n", curr_dir);
+
+    if(curr_command->argc > 1){
+        chdir(curr_command->argv[1]);
+        //FOR DEBUGGING: char *curr_dir = getcwd(cwd, sizeof(cwd));
+        //FOR DEBUGGING: printf("Directory after argv[1]: %s", curr_dir);
+
+    } else if (curr_command->argc == 1){
+        chdir(getenv("HOME"));
+        //char *curr_dir = getcwd(cwd, sizeof(cwd));
+        //printf("Directory after argv[1]: %s", curr_dir);
+    } else {
+        printf("build_in_cd failed\n");
+        return;
+    }
+
+    //printf("%s", curr_dir);
+}
+
+
+void built_in_status(){
+    printf("Exit value: %d\n", status);
+}
 
 
 int main()
@@ -61,16 +89,15 @@ int main()
     struct command_line *curr_command;
     while(true)
     {
-        
         curr_command = parse_input();
 
         if (strcmp(curr_command->argv[0], "exit") == 0){
             built_in_exit();
         } else if (strcmp(curr_command->argv[0], "cd") == 0){
             built_in_cd(curr_command);
-        } //else if (strcmp(curr_command->argv[0], "status") == 0){
-            //built_in_status(curr_command);
-        //} //else {
+        } else if (strcmp(curr_command->argv[0], "status") == 0){
+            built_in_status(curr_command);
+        } //else {
             // calls external commands
         //}  
 
